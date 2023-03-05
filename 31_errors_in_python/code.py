7039fec36026b9b3a8a23c0817cabc14ee4bdfea
KingTugce <kingstugce@gmail.com>
2023-03-05 01:15:13 -0800

    update

---
+++ 31_errors_in_python/code.py
@@ -0,0 +1,28 @@
+def divide(dividend,divisor):
+    if divisor == 0:
+        raise ZeroDivisionError("Divisor cannot be 0.")
+    
+    return dividend / divisor
+
+
+
+Student = [
+    {"name":"Bob", "grades": [99,33,86,98,100]},
+    {"name":"Rolf", "grades": [98]},
+    {"name":"Sarah","grades": [98,78,45,87,99]},
+]
+
+print("Welcome to the average grade program. ")
+try:
+    for students in Student:
+        name = students["name"]
+        grades = students["grades"]
+        average = divide(sum(grades), len(grades))
+        print(f"{name} averaged {average}. ")
+        
+except ZeroDivisionError as e:
+    print("There are no grades yet in the list.")
+else:
+    print(f"The average grade is {average}.")
+finally:
+    print("Thank you")
