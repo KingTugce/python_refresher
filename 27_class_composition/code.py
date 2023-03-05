e2cc1df6243b5f8b920fec910b1b906183d2dff8
KingTugce <kingstugce@gmail.com>
2023-03-04 19:57:36 -0800

    update

--- 27_class_composition/code.py
+++ 27_class_composition/code.py
@@ -4,7 +4,7 @@ class BookShelf:
 
     def __str__(self):
         return f"BookShelf with {len(self.books)} books."
-    
+
 
 class Book:
     def __init__(self, name):
@@ -13,8 +13,7 @@ class Book:
     def __str__(self):
         return f"Book {self.name}"
     
-    book = Book("Harry Potter")
-    book2 = Book("Python 101")    
-    shelf = BookShelf(book, book2)
-
-    print(shelf) 
+book = Book("Harry Potter")
+book2 = Book("Python 101")    
+shelf = BookShelf(book, book2)
+print(shelf) 
