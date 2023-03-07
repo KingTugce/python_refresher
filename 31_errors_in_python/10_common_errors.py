bb112de9bc42683aedc90d7a3d4e28c66e38a9ae
KingTugce <kingstugce@gmail.com>
2023-03-07 15:56:28 -0800

    update

---
+++ 31_errors_in_python/10_common_errors.py
@@ -0,0 +1,19 @@
+# 1) AttributeError
+
+# 2) AssertionError
+
+# 3) ImportError
+
+# 4) SyntaxError
+
+# 5) TypeError
+
+# 6) IndexError
+
+# 7) NameError
+
+# 8) RecursionError
+
+# 9) IndentationError
+
+# 10) ValueError
